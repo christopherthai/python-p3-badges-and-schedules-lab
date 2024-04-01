b2a1b3e19f1eda0dce180b26db5692d4fe0e363c
Christopher Thai <christopherthai.nyli@gmail.com>
2024-04-01 17:31:18 -0400

    complete lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,23 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
+
 
 def batch_badge_creator(names):
-    return None
+    return [f"Hello, my name is {name}." for name in names]
+
 
 def assign_rooms(names):
-    return None
+    rooms = range(1, 9)
+
+    return [
+        f"Hello, {name}! You'll be assigned to room {room}!"
+        for name, room in zip(names, rooms)
+    ]
+
 
 def printer(names):
-    return None
+    for badge in batch_badge_creator(names):
+        print(badge)
+
+    for room in assign_rooms(names):
+        print(room)
